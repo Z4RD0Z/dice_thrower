ca1af4685e0fdbb91fa1eae5c01c78668bf7f197
Andrea Lomazzo <andrea_l@outlook.it>
2021-03-25 18:27:43 +0100

    feat:create function for parse dice string, change laungh_dice function

--- dice_throwers.py
+++ dice_throwers.py
@@ -3,16 +3,24 @@ import re
 
 
 class DiceThrower(object):
-    """
-  docstring
-  """
+    def __init__(self, game_type):
+        self.game_type = game_type
+
     def parse_dice_string(self, dice_string):
 
         dice_number = re.findall(r"(\d+)d", dice_string)
         dice_type = re.findall(r"d(\d+)", dice_string)
-        dice_number_type = zip(dice_number, dice_type)
+        dice_sign = re.findall(r"[\+\.][1-9]", dice_string)
+        if dice_sign == []:
+            dice_sign.append(0)
+
+        dice_number_type = zip(dice_number, dice_type, dice_sign)
 
         result = self.launch_dice(dice_number_type)
+
+        if self.game_type == "savage":
+            if "cr" not in dice_string and "dm" not in dice_string:
+                result.append(f"destiny dice -> {random.randrange(1, 7)}")
 
         return result
 
@@ -20,21 +28,33 @@ class DiceThrower(object):
 
         result = []
 
-        for number, d_type in dice_number_type:
-
+        for number, d_type, d_sign in dice_number_type:
+            print(f"{number},{d_type},{d_sign}")
             if number != None and d_type != None:
+
                 for n in range(0, int(number)):
                     if d_type == "4":
-                        result.append(random.randrange(1, 5))
+                        dice_result = random.randrange(1, 5)
                     if d_type == "6":
-                        result.append(random.randrange(1, 7))
+                        dice_result = random.randrange(1, 7)
                     if d_type == "8":
-                        result.append(random.randrange(1, 9))
+                        dice_result = random.randrange(1, 9)
                     if d_type == "10":
-                        result.append(random.randrange(1, 11))
+                        dice_result = random.randrange(1, 11)
                     if d_type == "12":
-                        result.append(random.randrange(1, 13))
+                        dice_result = random.randrange(1, 13)
                     if d_type == "20":
-                        result.append(random.randrange(1, 21))
+                        dice_result = random.randrange(1, 21)
 
+                    if d_sign != 0:
+                        if '+' in d_sign:
+                            print(dice_result)
+                            result.append(dice_result +
+                                          int(d_sign.replace("+", "")))
+                        elif '-' in d_sign:
+                            print(dice_result)
+                            result.append(dice_result -
+                                          int(d_sign.replace("-", "")))
+                    else:
+                        result.append(dice_result)
         return result
