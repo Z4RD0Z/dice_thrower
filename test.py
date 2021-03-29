aeef94ed832f01d7336eb4ce6d36652f067e74c2
Z4RD0Z <elaphe@outlook.it>
2021-03-29 21:27:25 +0200

    fix:import forgotten tests

---
+++ test.py
@@ -0,0 +1,44 @@
+from dice_throwers import DiceThrowerFactory, SavageDiceThrower
+from collections import Counter
+
+
+def test_create_SavageThrower(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
+
+    assert isinstance(dice_thrower, SavageDiceThrower)
+
+def test_result_is_list(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
+    result = dice_thrower.parse_dice_string("1d6")
+
+    assert isinstance(result, list) == True
+
+def test_has_destinyDice(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
+    result = dice_thrower.parse_dice_string("1d6")
+    assert any("destiny dice" in string for string in result)
+
+def test_check_total_without_mod(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
+    result = dice_thrower.parse_dice_string("2d6")
+    assert any("total without mod" in string for string in result)
+    assert sum('total without mod' in string for string in result) == 1
+
+def test_check_total_with_mod(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
+    result = dice_thrower.parse_dice_string("2d6+1")
+    assert any("total with mod" in string for string in result)
+    assert sum('total with mod' in string for string in result) == 1
+
+def test_multiple_dice_type_without_mod(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
+    result = dice_thrower.parse_dice_string("2d6+1 2d4")
+    assert any("total with mod" in string for string in result)
+    assert sum('total with mod' in string for string in result) == 1
+
+
+def test_multiple_dice_type_with_mod(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
+    result = dice_thrower.parse_dice_string("2d6+1 2d4+1")
+    assert any("total with mod" in string for string in result)
+    assert sum('total with mod' in string for string in result) >= 2
