d977f9b7f40c3e87dba6ad8e1ffcb1d74c8fd3e6
Andrea Lomazzo <andrea_l@outlook.it>
2021-03-29 12:34:37 +0200

    feat:create game type list

---
+++ cli.py
@@ -0,0 +1,7 @@
+import inquirer
+type_list = [
+  inquirer.List('game',
+                message="A cosa stai nerdando ? ",
+                choices=['Savage', 'CoC', 'Vampire', '1572', 'simple'],
+            ),
+]
