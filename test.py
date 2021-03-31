4265b178dc32a91b4aa380b398d5823a730eb44d
Z4RD0Z <elaphe@outlook.it>
2021-03-31 14:18:59 +0200

    add test on cthulhu dice thrower

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-from dice_throwers import DiceThrowerFactory, SavageDiceThrower
+from dice_throwers import DiceThrowerFactory, SavageDiceThrower, Cthulhu
 from collections import Counter
 
 
-def test_create_SavageThrower(game_type):
-    dice_thrower = DiceThrowerFactory.create_thrower(game_type)
-
+def test_create_SavageThrower():
+    dice_thrower = DiceThrowerFactory.create_thrower("Savage")
     assert isinstance(dice_thrower, SavageDiceThrower)
+
+
+def test_create_CthulhuThrower():
+    dice_thrower = DiceThrowerFactory.create_thrower("Cthulhu")
+    assert isinstance(dice_thrower, CthulhuDiceThrower)
+
 
 def test_result_is_list(game_type):
     dice_thrower = DiceThrowerFactory.create_thrower(game_type)
@@ -13,10 +18,12 @@ def test_result_is_list(game_type):
 
     assert isinstance(result, list) == True
 
+
 def test_has_destinyDice(game_type):
     dice_thrower = DiceThrowerFactory.create_thrower(game_type)
     result = dice_thrower.parse_dice_string("1d6")
     assert any("destiny dice" in string for string in result)
+
 
 def test_check_total_without_mod(game_type):
     dice_thrower = DiceThrowerFactory.create_thrower(game_type)
@@ -24,11 +31,13 @@ def test_check_total_without_mod(game_type):
     assert any("total without mod" in string for string in result)
     assert sum('total without mod' in string for string in result) == 1
 
+
 def test_check_total_with_mod(game_type):
     dice_thrower = DiceThrowerFactory.create_thrower(game_type)
     result = dice_thrower.parse_dice_string("2d6+1")
     assert any("total with mod" in string for string in result)
     assert sum('total with mod' in string for string in result) == 1
+
 
 def test_multiple_dice_type_without_mod(game_type):
     dice_thrower = DiceThrowerFactory.create_thrower(game_type)
@@ -42,3 +51,10 @@ def test_multiple_dice_type_with_mod(game_type):
     result = dice_thrower.parse_dice_string("2d6+1 2d3+1")
     assert any("total with mod" in string for string in result)
     assert sum('total with mod' in string for string in result) >= 2
+
+
+def test_cthulhu_action_dice(game_type):
+    dice_thrower = DiceThrowerFactory.create_thrower("Cthulhu")
+    result = dice_thrower.parse_dice_string("action"):
+    assert any("result -> " in result)
+    
